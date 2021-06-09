150a2101f5decea59e7ef99d220c4ba6d88ba815
henrylemmon <henrylemmon@yahoo.com>
2021-06-09 16:58:38 -0700

    admin panel crud started

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,12 +10,12 @@ class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        dd('index on UsersController');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,52 @@
+@extends('templates.main')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1 class="float-start">Users</h1>
+            <a href="{{ route('admin.users.create') }}"
+               class="btn btn-sm btn-success float-end"
+               role="button">Create User</a>
+        </div>
+    </div>
+
+
+    <div class="card table-container-card">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-sm btn-primary" role="button">Edit</a>
+                        <button type="button"
+                           class="btn btn-sm btn-danger"
+                           onclick="event.preventDefault();
+                            document.getElementById('delete-user-form-{{ $user->id }}').submit()"
+                           role="button">Delete</button>
+                        <form
+                            method="POST"
+                            action="{{ route('admin.users.destroy', $user->id) }}"
+                            style="display:none;"
+                            id="delete-user-form-{{ $user->id }}"
+                        >
+                            @csrf
+                            @method('DELETE')
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
